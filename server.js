09e03402d28fabb37e83f1b6f17c3cb9bfb5b815
Erick Brandao <brandaoerick@MacBook-Air.local>
2022-07-18 20:26:52 -0300

    updates

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bcrypt = require("bcrypt-nodejs");
 const cors = require("cors");
-
 const app = express();
 
 // app.use(express.urlencoded({extended: false}));
